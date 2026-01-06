b94579fbe0a85a383d821fd00ba3bffb605b022d
maelds <ismaelds20@gmail.com>
2026-01-06 20:56:20 +0000

    Correções

--- desafio_war.c
+++ desafio_war.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h> 
 
-#define MAX_TERRITORIOS 5
-#define TAM_STRING 100
+#define MAX_TERRITORIOS 5 // Limitação para número de territórios
+#define TAM_STRING 100 // Limitação de tamanho da string
 
+//Abaixo segue a struct com as características de cada Território
 typedef struct {
     char nome[30];
     char cor[10];
@@ -11,5 +12,6 @@ typedef struct {
 } Territorio;
 
 int main(){
-
+    Territorio territorios[MAX_TERRITORIOS]; // Vetor do tipo Territorio permitindo um máximo de 5 valores
+    
 };
